40aa8351fa1cc739d1a7446d392440ce3d73c108
OvidioOswaldo <33989895+OvidioOswaldo@users.noreply.github.com>
2023-06-17 12:34:05 -0300

    SecondActivity.kt

---
+++ Codigo/SecondActivity.kt
@@ -0,0 +1,17 @@
+package co.ovidioswaldo.projeto.porah
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+    }
+}
