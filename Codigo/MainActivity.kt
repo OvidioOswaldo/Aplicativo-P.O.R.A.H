1b225c4b411326b08054976a282857dc8715876a
OvidioOswaldo <33989895+OvidioOswaldo@users.noreply.github.com>
2023-06-20 09:02:59 -0300

    MainActivity.kt

--- Codigo/MainActivity.kt
+++ Codigo/MainActivity.kt
@@ -1,61 +1,24 @@
-package co.tiagoaguiar.tutorial.myfood
+package co.ovidiooswaldo.projeto.porah
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import co.ovidiooswaldo.proejoto.porah.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
-  private lateinit var binding: ActivityMainBinding
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(binding.root)
+    setContentView(R.layout.activity_main)
 
     val button: Button = findViewById(R.id.botao_com_id)
     button.setOnClickListener {
+      Toast.makeText(this, "Projeto da ORganização da Alimentação Humana", Toast.LENGTH_SHORT).show()
       openNextActivity()
     }
-
-    setupViews()
   }
-
   private fun openNextActivity() {
     val intent = Intent(this, SecondActivity::class.java)
     startActivity(intent)
   }
-
-  private fun setupViews() {
-    val tabLayout = binding.addTab
-    val viewPager = binding.addViewpager
-    val adapter = TabViewPagerAdapter(this)
-    viewPager.adapter = adapter
-
-    TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-      tab.text = getString(adapter.tabs[position])
-    }.attach()
-  }
 }
-
-class TabViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
-
-  val tabs = arrayOf(R.string.Pacientes, R.string.Prato, R.string.Refeicoes)
-  val fragments = arrayOf(MarketplaceFragment(), MarketplaceFragment(), MarketplaceFragment())
-
-  override fun getItemCount(): Int = fragments.size
-
-  override fun createFragment(position: Int): Fragment {
-    return fragments[position]
-  }
-}
-
-class MarketplaceFragment : Fragment() {}
