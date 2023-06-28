1e0611362db775101fc6bfe2bbde62aacf8326c2
OvidioOswaldo <33989895+OvidioOswaldo@users.noreply.github.com>
2023-06-28 08:43:50 -0300

    Add files via upload

---
+++ projetoporah/JakeActivity.java
@@ -0,0 +1,13 @@
+package com.ovidiooswaldo.projetoporah;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class JakeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jake);
+
+    }
+}
